18a72e0e08e8a77592611c555563da79b83ceb16
quantumbr <bilal@innostark.com>
2016-01-27 23:12:52 +0000

    Updated the database project and web projects connection string to point to cp.bilalrehman.com

--- FRS.Database/dbo/Tables/Menu.sql
+++ FRS.Database/dbo/Tables/Menu.sql
@@ -9,10 +9,7 @@
     [PermissionKey]        NVARCHAR (MAX) NULL,
     [IsRootItem]           BIT            NOT NULL,
     [ParentItem_MenuId]    INT            NULL,
-    [MenuTitleArbic]       NVARCHAR (250) NULL,
     CONSTRAINT [PK_Menu] PRIMARY KEY CLUSTERED ([MenuId] ASC),
     CONSTRAINT [RefMenu01] FOREIGN KEY ([ParentItem_MenuId]) REFERENCES [dbo].[Menu] ([MenuId])
 );
-
-
 
