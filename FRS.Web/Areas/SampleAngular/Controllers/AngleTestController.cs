a91a746692bb1dcd410cb10110f4fe05e8f7614f
Hassan <hassan@innostark.com>
2016-02-01 19:59:46 +0500

    Layout set Angle

---
+++ FRS.Web/Areas/SampleAngular/Controllers/AngleTestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FRS.Web.Areas.SampleAngular.Controllers
+{
+    public class AngleTestController : Controller
+    {
+        // GET: SampleAngular/AngleTest
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
