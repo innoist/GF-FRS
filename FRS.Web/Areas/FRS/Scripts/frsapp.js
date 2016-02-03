3ab2eb562aaf3e15162241c74c6687643ef4d9a4
istammar <ammar@innostark.com>
2016-02-03 12:47:51 +0500

    template setting

--- FRS.Web/Areas/FRS/Scripts/frsapp.js
+++ FRS.Web/Areas/FRS/Scripts/frsapp.js
@@ -1,27 +1,18 @@
 ﻿//(function () {
 //    'use strict';
 
-//    angular
-//        .module('asdf', []);
+//    angular.module('myModule', []);
 //})();
 
 //(function () {
 //    'use strict';
 
-//    angular
-//        .module('FRS.sidebar', []);
-//})();
+//    angular.module('myModule').controller('SidebarController', SidebarController);
 
-//(function () {
-//    'use strict';
+//    SidebarController.$inject = ['$rootScope', '$scope', 'SidebarLoader'];
 
-//    angular
-//        .module('FRS.sidebar')
-//        .controller('SidebarController', SidebarController);
-
-//    SidebarController.$inject = ['$rootScope', '$scope', '$state', 'SidebarLoader', 'Utils'];
-//    function SidebarController($rootScope, $scope, $state, SidebarLoader, Utils) {
-
+//    function SidebarController($rootScope, $scope, SidebarLoader) {
+//        $scope.FirstName = "Ammar";
 //        activate();
 
 //        ////////////////
@@ -116,16 +107,15 @@
 //                return (typeof $index === 'string') && !($index.indexOf('-') < 0);
 //            }
 
-//        } // activate
+//        }
 //    }
-
 //})();
 
 //(function () {
 //    'use strict';
 
 //    angular
-//        .module('FRS.sidebar')
+//        .module('myModule')
 //        .service('SidebarLoader', SidebarLoader);
 
 //    SidebarLoader.$inject = ['$http'];
@@ -152,132 +142,113 @@
 //    'use strict';
 
 //    angular
-//        .module('FRS.utils')
-//        .service('Utils', Utils);
+//        .module('app.dashboard')
+//        .controller('DashboardV2Controller', DashboardV2Controller);
 
-//    Utils.$inject = ['$window', 'APP_MEDIAQUERY'];
-//    function Utils($window, APP_MEDIAQUERY) {
+//    DashboardV2Controller.$inject = ['$rootScope', '$scope', '$state'];
+//    function DashboardV2Controller($rootScope, $scope, $state) {
+//        var vm = this;
 
-//        var $html = angular.element('html'),
-//            $win = angular.element($window),
-//            $body = angular.element('body');
+//        activate();
 
-//        return {
-//            // DETECTION
-//            support: {
-//                transition: (function () {
-//                    var transitionEnd = (function () {
+//        ////////////////
 
-//                        var element = document.body || document.documentElement,
-//                            transEndEventNames = {
-//                                WebkitTransition: 'webkitTransitionEnd',
-//                                MozTransition: 'transitionend',
-//                                OTransition: 'oTransitionEnd otransitionend',
-//                                transition: 'transitionend'
-//                            }, name;
+//        function activate() {
 
-//                        for (name in transEndEventNames) {
-//                            if (element.style[name] !== undefined) return transEndEventNames[name];
-//                        }
-//                    }());
-
-//                    return transitionEnd && { end: transitionEnd };
-//                })(),
-//                animation: (function () {
-
-//                    var animationEnd = (function () {
-
-//                        var element = document.body || document.documentElement,
-//                            animEndEventNames = {
-//                                WebkitAnimation: 'webkitAnimationEnd',
-//                                MozAnimation: 'animationend',
-//                                OAnimation: 'oAnimationEnd oanimationend',
-//                                animation: 'animationend'
-//                            }, name;
-
-//                        for (name in animEndEventNames) {
-//                            if (element.style[name] !== undefined) return animEndEventNames[name];
-//                        }
-//                    }());
-
-//                    return animationEnd && { end: animationEnd };
-//                })(),
-//                requestAnimationFrame: window.requestAnimationFrame ||
-//                                       window.webkitRequestAnimationFrame ||
-//                                       window.mozRequestAnimationFrame ||
-//                                       window.msRequestAnimationFrame ||
-//                                       window.oRequestAnimationFrame ||
-//                                       function (callback) { window.setTimeout(callback, 1000 / 60); },
-//                /*jshint -W069*/
-//                touch: (
-//                    ('ontouchstart' in window && navigator.userAgent.toLowerCase().match(/mobile|tablet/)) ||
-//                    (window.DocumentTouch && document instanceof window.DocumentTouch) ||
-//                    (window.navigator['msPointerEnabled'] && window.navigator['msMaxTouchPoints'] > 0) || //IE 10
-//                    (window.navigator['pointerEnabled'] && window.navigator['maxTouchPoints'] > 0) || //IE >=11
-//                    false
-//                ),
-//                mutationobserver: (window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver || null)
-//            },
-//            // UTILITIES
-//            isInView: function (element, options) {
-//                /*jshint -W106*/
-//                var $element = $(element);
-
-//                if (!$element.is(':visible')) {
-//                    return false;
-//                }
-
-//                var window_left = $win.scrollLeft(),
-//                    window_top = $win.scrollTop(),
-//                    offset = $element.offset(),
-//                    left = offset.left,
-//                    top = offset.top;
-
-//                options = $.extend({ topoffset: 0, leftoffset: 0 }, options);
-
-//                if (top + $element.height() >= window_top && top - options.topoffset <= window_top + $win.height() &&
-//                    left + $element.width() >= window_left && left - options.leftoffset <= window_left + $win.width()) {
-//                    return true;
-//                } else {
-//                    return false;
-//                }
-//            },
-
-//            langdirection: $html.attr('dir') === 'rtl' ? 'right' : 'left',
-
-//            isTouch: function () {
-//                return $html.hasClass('touch');
-//            },
-
-//            isSidebarCollapsed: function () {
-//                return $body.hasClass('aside-collapsed');
-//            },
-
-//            isSidebarToggled: function () {
-//                return $body.hasClass('aside-toggled');
-//            },
-
-//            isMobile: function () {
-//                return $win.width() < APP_MEDIAQUERY.tablet;
+//            // Change layout mode
+//            if ($state.includes('app-h')) {
+//                // Setup layout horizontal for demo
+//                $rootScope.app.layout.horizontal = true;
+//                $scope.$on('$destroy', function () {
+//                    $rootScope.app.layout.horizontal = false;
+//                });
+//            }
+//            else {
+//                $rootScope.app.layout.isCollapsed = true;
 //            }
 
-//        };
+//            // BAR STACKED
+//            // ----------------------------------- 
+//            vm.barStackedOptions = {
+//                series: {
+//                    stack: true,
+//                    bars: {
+//                        align: 'center',
+//                        lineWidth: 0,
+//                        show: true,
+//                        barWidth: 0.6,
+//                        fill: 0.9
+//                    }
+//                },
+//                grid: {
+//                    borderColor: '#eee',
+//                    borderWidth: 1,
+//                    hoverable: true,
+//                    backgroundColor: '#fcfcfc'
+//                },
+//                tooltip: true,
+//                tooltipOpts: {
+//                    content: function (label, x, y) { return x + ' : ' + y; }
+//                },
+//                xaxis: {
+//                    tickColor: '#fcfcfc',
+//                    mode: 'categories'
+//                },
+//                yaxis: {
+//                    min: 0,
+//                    max: 200, // optional: use it for a clear represetation
+//                    position: ($rootScope.app.layout.isRTL ? 'right' : 'left'),
+//                    tickColor: '#eee'
+//                },
+//                shadowSize: 0
+//            };
+
+//            // SPLINE
+//            // ----------------------------------- 
+
+//            vm.splineOptions = {
+//                series: {
+//                    lines: {
+//                        show: false
+//                    },
+//                    points: {
+//                        show: true,
+//                        radius: 4
+//                    },
+//                    splines: {
+//                        show: true,
+//                        tension: 0.4,
+//                        lineWidth: 1,
+//                        fill: 0.5
+//                    }
+//                },
+//                grid: {
+//                    borderColor: '#eee',
+//                    borderWidth: 1,
+//                    hoverable: true,
+//                    backgroundColor: '#fcfcfc'
+//                },
+//                tooltip: true,
+//                tooltipOpts: {
+//                    content: function (label, x, y) { return x + ' : ' + y; }
+//                },
+//                xaxis: {
+//                    tickColor: '#fcfcfc',
+//                    mode: 'categories'
+//                },
+//                yaxis: {
+//                    min: 0,
+//                    max: 150, // optional: use it for a clear represetation
+//                    tickColor: '#eee',
+//                    position: ($rootScope.app.layout.isRTL ? 'right' : 'left'),
+//                    tickFormatter: function (v) {
+//                        return v/* + ' visitors'*/;
+//                    }
+//                },
+//                shadowSize: 0
+//            };
+//        }
 //    }
-//})();
-
-//(function () {
-//    'use strict';
-
-//    angular
-//        .module('FRS.core')
-//        .constant('APP_MEDIAQUERY', {
-//            'desktopLG': 1200,
-//            'desktop': 992,
-//            'tablet': 768,
-//            'mobile': 480
-//        })
-//    ;
-
 //})();
 
 var mainApp = angular.module("FRSApp", []);
