0ae963a4ed8ea4b010b425a51c68259b3ef2344d
Rmjahangeer <callrmj@gmail.com>
2016-02-04 18:20:10 +0500

    val controller

--- FRS.WebApi/Controllers/ValuesController.cs
+++ FRS.WebApi/Controllers/ValuesController.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System.Collections.Generic;
 using System.Web.Http;
 
 namespace FRS.WebApi.Controllers
 {
-    //[Authorize]
+    [Authorize]
     public class ValuesController : ApiController
     {
         // GET api/values
@@ -20,21 +16,6 @@ namespace FRS.WebApi.Controllers
         public string Get(int id)
         {
             return "value";
-        }
-
-        // POST api/values
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
         }
     }
 }
