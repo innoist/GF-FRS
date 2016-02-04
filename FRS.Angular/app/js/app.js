852c3a7cb6f4bf8f08d004548722d8cbc72d992d
istammar <ammar@innostark.com>
2016-02-04 18:32:35 +0500

    call api from LoadMetaData view

--- FRS.Angular/app/js/app.js
+++ FRS.Angular/app/js/app.js
@@ -8269,7 +8269,7 @@
         ////////////////
 
         function getLoadMetaData(onReady, onError) {
-            var urlMetaData = window.istApiUrl + 'api/LoadMetaData';
+            var urlMetaData = window.frsApiUrl + '/api/LoadMetaData';
 
             onError = onError || function () { alert('Failure loading Meta Data'); };
 
