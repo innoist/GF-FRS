e0e4d199a18df46828ef74f218730d581211056a
Rmjahangeer <callrmj@gmail.com>
2016-02-15 16:35:25 +0500

    proj

---
+++ FRS.Commons/FRSApplicationRoles.cs
@@ -0,0 +1,23 @@
+﻿namespace FRS.Commons
+{
+    /// <summary>
+    /// Application Roles to be used across the application
+    /// </summary>
+    public sealed class FRSApplicationRoles
+    {
+        /// <summary>
+        /// System Administrator
+        /// </summary>
+        public static string SystemAdministrator = "SystemAdministrator";
+
+        /// <summary>
+        /// Admin
+        /// </summary>
+        public static string Admin = "Admin";
+
+        /// <summary>
+        /// User
+        /// </summary>
+        public static string User = "User";
+    }
+}
---
+++ FRS.Commons/FRSUserClaims.cs
@@ -0,0 +1,9 @@
+﻿
+namespace FRS.Commons
+{
+    public static class FRSUserClaims
+    {
+        public const string UserId = "http://schemas.xmlsoap.org/Cares/2014/12/identity/claims/UserId";
+        public const string UserTimeZoneOffset = "http://schemas.xmlsoap.org/Cares/2014/12/identity/claims/userTimeZoneOffset";
+    }
+}
