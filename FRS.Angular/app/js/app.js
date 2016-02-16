6c3e9ea13f2eb4778b525cf62bbb0c613f3dac4a
Rmjahangeer <callrmj@gmail.com>
2016-02-16 11:13:20 +0500

    menu

--- FRS.Angular/app/js/app.js
+++ FRS.Angular/app/js/app.js
@@ -9972,7 +9972,7 @@
         ////////////////
 
         function getMenu(onReady, onError) {
-            var menuJson = '../../server/sidebar-menu.js',
+            var menuJson = frsApiUrl +'/api/Menu',
                 menuURL = menuJson + '?v=' + (new Date().getTime()); // jumps cache
 
             onError = onError || function () { alert('Failure loading menu'); };
