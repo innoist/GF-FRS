2d51cf7efbca382f7e8cf6dfe2574ad7fee2ac01
Syed Usman <usman@innostark.com>
2016-03-22 18:51:48 +0500

    ReconciledMappingId

---
+++ FRS.WebApi/Models/ReconciledMapping/ReconciledMappingModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FRS.WebApi.Models.ReconciledMapping
+{
+    public class ReconciledMappingModel
+    {
+        public long ReconciledMappingId { get; set; }
+        public long OracleGLEntryId { get; set; }
+        public long MT940CustomerStatementTransactionId { get; set; }
+        public bool IsDeleted { get; set; }
+        public bool IsManual { get; set; }
+        public string CreatedBy { get; set; }
+        public System.DateTime CreatedOn { get; set; }
+        public string ModifiedBy { get; set; }
+        public System.DateTime ModifiedOn { get; set; }
+        public string AccountNumber { get; set; }
+
+        public string AccountDate { get; set; }
+        public string TransactionDate { get; set; }
+        public string Amount { get; set; }
+        public string DebitOrCredit { get; set; }
+        
+            
+            
+            
+            
+        
+      
+    }
+}
---
+++ FRS.WebApi/ViewModels/ReconciledMapping/ReconciledMappingViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using FRS.WebApi.Models.MT940Load;
+using FRS.WebApi.Models.ReconciledMapping;
+
+namespace FRS.WebApi.ViewModels.ReconciledMapping
+{
+    public class ReconciledMappingViewModel
+    {
+        public ReconciledMappingViewModel()
+        {
+            ReconciledMappingModels = new List<ReconciledMappingModel>();
+        }
+
+        public IEnumerable<ReconciledMappingModel> ReconciledMappingModels { get; set; }
+
+        public int TotalCount { get; set; }
+        public int TotalRecords { get; set; }
+        public int FilteredCount { get; set; }
+    }
+}
