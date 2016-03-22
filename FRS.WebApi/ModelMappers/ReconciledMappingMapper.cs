e2d6ae918c5fe4d30ba09ad6f655e9845d63285d
Syed Usman <usman@innostark.com>
2016-03-22 19:28:33 +0500

    mapping

---
+++ FRS.WebApi/ModelMappers/ReconciledMappingMapper.cs
@@ -0,0 +1,25 @@
+﻿using FRS.Models.DomainModels;
+using FRS.WebApi.Models.MT940Balance;
+
+namespace FRS.WebApi.ModelMappers
+{
+    public static class ReconciledMappingMapper
+    {
+        //public static MT940BalanceModel MapFromServerToClient(this MT940Balance source)
+        //{
+        //    return new MT940BalanceModel
+        //    {
+        //        Value = source.Value,
+        //        ModifiedBy = source.ModifiedBy,
+        //        CreatedBy = source.CreatedBy,
+        //        CreatedOn = source.CreatedOn,
+        //        ModifiedOn = source.ModifiedOn,
+        //        DebitOrCredit = source.DebitOrCredit == "C" ? "Credit" : source.DebitOrCredit == "D" ? "Debit": "N/A",
+        //        Currency = source.Currency.Sign + "(" + source.Currency.Name + ")",
+        //        CurrencyId = source.CurrencyId,
+        //        EntryDate = source.EntryDate.ToString("dd-MMM-yy"),
+        //        MT940BalanceId = source.MT940BalanceId
+        //    };
+        //}
+    }
+}
